13474986d643e89cee4cd8d314bf6164615c45f9
zhaizhengqing <zhaizhengqing@outlook.com>
2023-11-21 17:07:20 +0800

    init

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {/* Add this <Head> tag */}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
+            {/* Keep the existing code here */}
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            <Date dateString={postData.date} />
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    );
+}
